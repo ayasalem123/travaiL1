47fc460575fa1427b877cc880223935b48bd6939
ayasalem123 <salem.aya184@gmail.com>
2023-01-20 21:00:02 +0100

    first

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const router=express.Router();
+const path=require("path")
+router.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../","pages","home.html"))
+})
+router.get("/contact",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../","pages","contact.html"))
+})
+router.get("/ourServices",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../","pages","service.html"))
+})
+module.exports=router;
